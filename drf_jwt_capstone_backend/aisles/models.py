da609587d254d0e609a69b80ac1affd543d57b51
Dillon Coleman <coleman399@gmail.com>
2021-12-16 16:07:53 -0700

    Migrated Aisle + Migrated Product to MySQL

--- drf_jwt_capstone_backend/aisles/models.py
+++ drf_jwt_capstone_backend/aisles/models.py
@@ -3,4 +3,7 @@ from django.db import models
 # Create your models here.
 class Aisle(models.Model):
     aisleId=models.AutoField(primary_key=True)
-    aisleId=models.CharField(max_length=10)
+    aisleName=models.CharField(max_length=10)
+    
+    def __str__(self):
+        return self.aisleName
